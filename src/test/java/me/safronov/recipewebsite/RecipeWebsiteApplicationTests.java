3b8d799800dd4227854b88f1c42d965b4ce0331b
79650 <last137954682@gmail.ru>
2023-03-12 01:34:42 +0300

    RecipeWebsite

---
+++ src/test/java/me/safronov/recipewebsite/RecipeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package me.safronov.recipewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
