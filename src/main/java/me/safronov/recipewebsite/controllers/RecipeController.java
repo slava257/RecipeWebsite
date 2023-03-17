15a2af5559bf6fcaaca9d18f645e7116c67f0b24
79650 <last137954682@gmail.ru>
2023-03-18 02:00:19 +0300

    API

--- src/main/java/me/safronov/recipewebsite/controllers/RecipeController.java
+++ src/main/java/me/safronov/recipewebsite/controllers/RecipeController.java
@@ -1,13 +1,14 @@
 package me.safronov.recipewebsite.controllers;
-
-
-
 import me.safronov.recipewebsite.DTO.RecipeDTO;
+import me.safronov.recipewebsite.exception.RecipeNotFoundException;
 import me.safronov.recipewebsite.model.Recipe;
 import me.safronov.recipewebsite.services.impl.RecipeImplServices;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/recipe")
 //Создайте контроллеры и API для создания и получения рецептов и ингредиентов.
 public class RecipeController {
     private final RecipeImplServices recipeImplServices;
@@ -16,17 +17,42 @@ public class RecipeController {
         this.recipeImplServices = recipeImplServices;
     }
 
-    @PostMapping("/recipe")
+    @PostMapping()
 
     public RecipeDTO addRecipe(@RequestBody Recipe recipe) {
 
         return recipeImplServices.addRecipe(recipe);
     }
 
-    @GetMapping("/recipe/{count}")
+    @GetMapping("/{count}")
 
     public RecipeDTO getRecipes(@PathVariable("count") int count) {
 
         return recipeImplServices.getRecipes(count);
     }
+
+    //Продолжаем работать с приложением по рецептам.
+    // К уже созданной структуре добавьте операции редактирования и удаления рецептов и ингредиентов.
+    @PutMapping("/{count}")
+    public RecipeDTO editRecipe(@PathVariable int count, @RequestBody Recipe recipe) {
+        RecipeDTO recipeDTO = recipeImplServices.editRecipe(count,recipe);
+        if (recipeDTO != null) {
+            return recipeImplServices.editRecipe(count, recipe);
+        }
+        throw new RecipeNotFoundException();
+    }
+
+    @DeleteMapping("/{count}")
+    public RecipeDTO deleteRecipe(@PathVariable int count) {
+        RecipeDTO recipeDTO = recipeImplServices.deleteRecipe(count);
+        if (recipeDTO != null) {
+            return recipeImplServices.deleteRecipe(count);
+        }
+        throw new RecipeNotFoundException();
+    }
+    @GetMapping()
+    public List<RecipeDTO> allGetRecipe() {
+        return recipeImplServices.allGetRecipes();
+    }
+
 }
--- src/main/java/me/safronov/recipewebsite/services/impl/RecipeImplServices.java
+++ src/main/java/me/safronov/recipewebsite/services/impl/RecipeImplServices.java
@@ -1,11 +1,17 @@
 package me.safronov.recipewebsite.services.impl;
 
+
 import me.safronov.recipewebsite.DTO.RecipeDTO;
+import me.safronov.recipewebsite.exception.IngredientsNotFoundException;
+import me.safronov.recipewebsite.exception.RecipeNotFoundException;
+import me.safronov.recipewebsite.model.Ingredients;
 import me.safronov.recipewebsite.model.Recipe;
 import org.springframework.stereotype.Service;
 
 
 import java.util.*;
+
+
 //В сервисе должны быть реализованы методы:
 //1. Добавления нового рецепта.
 //В метод передается заполненный объект класса рецепта, который сохраняется в карте и получает свой порядковый
@@ -17,22 +23,56 @@ import java.util.*;
 //В сервисе должны быть методы для добавления нового ингредиента и получения его по идентификатору.
 // Можно делать по аналогии с сервисом рецептов.
 @Service
-public class RecipeImplServices  {
-    private  int count = 0;
+public class RecipeImplServices {
+    private int count = 0;
+    private final IngredientsImplServices ingredientsImplServices;
     private final Map<Integer, Recipe> recipes = new HashMap<>();
+
+    public RecipeImplServices(IngredientsImplServices ingredientsImplServices) {
+        this.ingredientsImplServices = ingredientsImplServices;
+    }
 
 
     public RecipeDTO addRecipe(Recipe recipe) {
         recipes.put(count++, recipe);
-        return RecipeDTO.from(count,recipe);
+        for (Ingredients ingredients : recipe.getIngredients()) {
+            this.ingredientsImplServices.addIngredients(ingredients);
+        }
+        return RecipeDTO.from(count, recipe);
     }
+
 
 
     public RecipeDTO getRecipes(int count) {
         Recipe recipe = recipes.get(count);
         if (recipe != null) {
-            return RecipeDTO.from(count,recipe);
+            return RecipeDTO.from(count, recipe);
         }
         return null;
+    }
+
+    public RecipeDTO editRecipe(int count, Recipe recipe) {
+        if (recipes.containsKey(count)) {
+            recipes.put(count, recipe);
+            return RecipeDTO.from(count, recipe);
+        }
+        throw new RecipeNotFoundException();
+    }
+
+    public RecipeDTO deleteRecipe(int count) {
+        Recipe recipe = recipes.get(count);
+        if (recipes.containsKey(count)) {
+            recipes.remove(count);
+            return RecipeDTO.from(count, recipe);
+        }
+        throw new IngredientsNotFoundException();
+    }
+    public List<RecipeDTO> allGetRecipes() {
+       List<RecipeDTO> result =new ArrayList<>();
+       for(Map.Entry<Integer,Recipe> entry : recipes.entrySet()){
+           result.add(RecipeDTO.from(entry.getKey(),entry.getValue()));
+
+        }
+        return result;
     }
 }
